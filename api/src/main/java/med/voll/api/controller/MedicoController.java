6c3c8759fcb1f6aea257f62ececc5479078aed9c
William Soares <pedro.williamsoares2022@gmail.com>
2026-03-03 14:33:52 -0300

    feat: adiciona endpoint GET /medicos/listar para listar médicos cadastrados

--- api/src/main/java/med/voll/api/controller/MedicoController.java
+++ api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,14 +1,19 @@
 package med.voll.api.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
 import med.voll.api.medico.DadosCadastroMedico;
 import med.voll.api.medico.Medico;
+import med.voll.api.medico.MedicoListagemDTO;
 import med.voll.api.repository.MedicoRepository;
 
 @RestController
@@ -20,7 +25,12 @@ public class MedicoController {
 
   @PostMapping
   @Transactional
-  public void cadastrarMedico(@RequestBody DadosCadastroMedico dados) {
+  public void cadastrarMedico(@RequestBody @Valid DadosCadastroMedico dados) {
     repository.save(new Medico(dados));
+  }
+
+  @GetMapping("/listar")
+  public List<MedicoListagemDTO> listarMedicos() {
+    return repository.findAll().stream().map(MedicoListagemDTO::new).toList();
   }
 }
