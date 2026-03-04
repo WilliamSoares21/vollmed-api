8627b2f80d70c695a8aafcbe61fb4f8f02a83eba
William <pedro.williamsoares2022@gmail.com>
2026-03-04 11:13:43 -0300

    feat(medico): substitui List por Page para implementar paginação no endpoint de listagem
    - Substituir List<MedicoListagemDTO> por Page<MedicoListagemDTO>
    - Adicionar parâmetro Pageable para suportar parâmetros de consulta como
    página, tamanho e ordenação
    - Usar o método map() do Spring Data para converter Page<Medico> em
    Page<MedicoListagemDTO>

--- api/src/main/java/med/voll/api/controller/MedicoController.java
+++ api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -3,6 +3,8 @@ package med.voll.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -30,7 +32,7 @@ public class MedicoController {
   }
 
   @GetMapping("/listar")
-  public List<MedicoListagemDTO> listarMedicos() {
-    return repository.findAll().stream().map(MedicoListagemDTO::new).toList();
+  public Page<MedicoListagemDTO> listarMedicos(Pageable paginacao) {
+    return repository.findAll(paginacao).map(MedicoListagemDTO::new);
   }
 }
