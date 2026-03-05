1234a653d658cfb84d401b5e72e6b5750f8b0ee3
William Soares <pedro.williamsoares2022@gmail.com>
2026-03-05 14:39:51 -0300

    refactor(medico): adiciona id ao DTO de listagem

--- api/src/main/java/med/voll/api/medico/MedicoListagemDTO.java
+++ api/src/main/java/med/voll/api/medico/MedicoListagemDTO.java
@@ -1,13 +1,14 @@
 package med.voll.api.medico;
 
 public record MedicoListagemDTO(
+    Long id,
     String nome,
     String email,
     String crm,
     Especialidade especialidade) {
 
   public MedicoListagemDTO(Medico medico) {
-    this(medico.getNome(), medico.getEmail(), medico.getCrm(),
+    this(medico.getId(), medico.getNome(), medico.getEmail(), medico.getCrm(),
         medico.getEspecialidade());
   }
 }
