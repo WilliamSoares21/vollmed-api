8133e565a6e4478d9c3bf2b9d07524cf5ad3cbb8
William <pedro.williamsoares2022@gmail.com>
2026-03-09 16:11:28 -0300

    feat(db): criar migration para tabela de pacientes

---
+++ api/src/main/resources/db/migration/V5__create-table-pacientes.sql
@@ -0,0 +1,19 @@
+CREATE TABLE pacientes(
+
+    id bigint NOT NULL auto_increment,
+    nome varchar(100) NOT NULL,
+    email varchar(100) NOT NULL UNIQUE,
+    cpf varchar(14) NOT NULL UNIQUE,
+    telefone varchar(20) NOT NULL,
+    logradouro varchar(100) NOT NULL,
+    bairro varchar(100) NOT NULL,
+    cep varchar(9) NOT NULL,
+    complemento varchar(100),
+    numero varchar(20),
+    uf char(2) NOT NULL,
+    cidade varchar(100) NOT NULL,
+    ativo tinyint NOT NULL,
+
+    PRIMARY KEY(id)
+
+);
