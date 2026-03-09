bb8b99ef5129ecd7b787d7d124efdff8666bbd72
William <pedro.williamsoares2022@gmail.com>
2026-03-09 16:12:14 -0300

    feat(paciente): criar entidade e DTOs do domínio de paciente

---
+++ api/src/main/java/med/voll/api/domain/paciente/DetalhamentoPacienteDTO.java
@@ -0,0 +1,17 @@
+package med.voll.api.domain.paciente;
+
+import med.voll.api.domain.endereco.Endereco;
+
+public record DetalhamentoPacienteDTO(
+        Long id,
+        String nome,
+        String email,
+        String cpf,
+        String telefone,
+        Endereco endereco) {
+
+    public DetalhamentoPacienteDTO(Paciente paciente) {
+        this(paciente.getId(), paciente.getNome(), paciente.getEmail(),
+                paciente.getCpf(), paciente.getTelefone(), paciente.getEndereco());
+    }
+}
---
+++ api/src/main/java/med/voll/api/domain/paciente/Paciente.java
@@ -0,0 +1,88 @@
+package med.voll.api.domain.paciente;
+
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.Valid;
+import med.voll.api.domain.endereco.Endereco;
+
+@Entity(name = "Paciente")
+@Table(name = "pacientes")
+public class Paciente {
+
+    protected Paciente() {
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Boolean ativo;
+
+    private String nome;
+
+    private String email;
+
+    private String telefone;
+
+    private String cpf;
+
+    @Embedded
+    private Endereco endereco;
+
+    public Paciente(DadosCadastroPaciente dados) {
+        this.ativo = true;
+        this.nome = dados.nome();
+        this.email = dados.email();
+        this.telefone = dados.telefone();
+        this.cpf = dados.cpf();
+        this.endereco = new Endereco(dados.endereco());
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Boolean getAtivo() {
+        return ativo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void atualizarInformacoes(@Valid DadosAtualizacaoPaciente dados) {
+        if (dados.nome() != null) {
+            this.nome = dados.nome();
+        }
+        if (dados.telefone() != null) {
+            this.telefone = dados.telefone();
+        }
+        if (dados.endereco() != null) {
+            this.endereco.atualizarInformacoes(dados.endereco());
+        }
+    }
+
+    public void excluir() {
+        this.ativo = false;
+    }
+}
