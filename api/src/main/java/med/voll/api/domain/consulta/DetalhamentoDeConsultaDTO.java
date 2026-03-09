b96c848557b827b832e55bd240d44b287c826f41
William <pedro.williamsoares2022@gmail.com>
2026-03-09 16:12:59 -0300

    feat(consulta): criar entidade, DTOs e repositório do domínio de consulta

---
+++ api/src/main/java/med/voll/api/domain/consulta/DetalhamentoDeConsultaDTO.java
@@ -0,0 +1,17 @@
+package med.voll.api.domain.consulta;
+
+import java.time.LocalDateTime;
+
+public record DetalhamentoDeConsultaDTO(
+    Long id,
+    Long idMedico,
+    Long idPaciente,
+    LocalDateTime data) {
+
+  public DetalhamentoDeConsultaDTO(Consulta consulta) {
+    this(consulta.getId(),
+        consulta.getMedico().getId(),
+        consulta.getPaciente().getId(),
+        consulta.getData());
+  }
+}
