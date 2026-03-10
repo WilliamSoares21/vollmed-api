641ed609d95a6d1489ba35323e415c06708da5de
William <pedro.williamsoares2022@gmail.com>
2026-03-09 21:06:06 -0300

    refactor(consulta): aplicar validadores, seleção automática de médico e validação de existência de entidades

--- api/src/main/java/med/voll/api/service/ConsultaService.java
+++ api/src/main/java/med/voll/api/service/ConsultaService.java
@@ -1,34 +1,58 @@
 package med.voll.api.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.validation.ValidationException;
 import med.voll.api.domain.consulta.AgendamentoDeConsultaDTO;
 import med.voll.api.domain.consulta.Consulta;
 import med.voll.api.domain.consulta.ConsultaRepository;
 import med.voll.api.domain.consulta.DetalhamentoDeConsultaDTO;
+import med.voll.api.domain.consulta.validacoes.ValidadorAgendamentoDeConsulta;
+import med.voll.api.domain.medico.Medico;
 import med.voll.api.repository.MedicoRepository;
 import med.voll.api.repository.PacienteRepository;
 
 @Service
 public class ConsultaService {
 
-    @Autowired
-    private ConsultaRepository consultaRepository;
+  @Autowired
+  private ConsultaRepository consultaRepository;
 
-    @Autowired
-    private MedicoRepository medicoRepository;
+  @Autowired
+  private MedicoRepository medicoRepository;
 
-    @Autowired
-    private PacienteRepository pacienteRepository;
+  @Autowired
+  private PacienteRepository pacienteRepository;
 
-    public DetalhamentoDeConsultaDTO agendar(AgendamentoDeConsultaDTO dados) {
-        var medico = medicoRepository.getReferenceById(dados.idMedico());
-        var paciente = pacienteRepository.getReferenceById(dados.idPaciente());
+  @Autowired
+  private List<ValidadorAgendamentoDeConsulta> validadores;
 
-        var consulta = new Consulta(null, medico, paciente, dados.data());
-        consultaRepository.save(consulta);
+  public DetalhamentoDeConsultaDTO agendar(AgendamentoDeConsultaDTO dados) {
+    validadores.forEach(v -> v.validar(dados));
 
-        return new DetalhamentoDeConsultaDTO(consulta);
+    var medico = escolherMedico(dados);
+
+    var paciente = pacienteRepository.findById(dados.idPaciente())
+        .orElseThrow(() -> new EntityNotFoundException("Paciente não encontrado com id: " + dados.idPaciente()));
+
+    var consulta = new Consulta(null, medico, paciente, dados.data());
+    consultaRepository.save(consulta);
+
+    return new DetalhamentoDeConsultaDTO(consulta);
+  }
+
+  public Medico escolherMedico(AgendamentoDeConsultaDTO dados) {
+    if (dados.idMedico() != null) {
+      return medicoRepository.getReferenceById(dados.idMedico());
     }
+    if (dados.especialidade() == null) {
+      throw new ValidationException("Especialidade é obrigatoria quando o médico não for escolhido");
+    }
+
+    return medicoRepository.escolherMedicoAleatorioLivreNaData(dados.especialidade(), dados.data());
+  }
 }
