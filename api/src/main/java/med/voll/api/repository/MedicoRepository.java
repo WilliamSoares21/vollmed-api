a578fb76283d840534bff54e31858a191ee78933
William Soares <pedro.williamsoares2022@gmail.com>
2026-03-10 14:38:02 -0300

    refactor(medico): renomear parâmetro de query para consistência

--- api/src/main/java/med/voll/api/repository/MedicoRepository.java
+++ api/src/main/java/med/voll/api/repository/MedicoRepository.java
@@ -40,5 +40,5 @@ public interface MedicoRepository extends JpaRepository<Medico, Long> {
       where
       m.id = :id
       """)
-  Boolean findAtivoById(Long idMedico);
+  Boolean findAtivoById(Long id);
 }
