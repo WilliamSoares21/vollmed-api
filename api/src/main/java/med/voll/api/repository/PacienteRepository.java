765338961577b5abfcd381c7ee1b9f0609a48d9c
William Soares <pedro.williamsoares2022@gmail.com>
2026-03-10 14:38:26 -0300

    refactor(paciente): adicionar query JPQL explícita para findAtivoById

--- api/src/main/java/med/voll/api/repository/PacienteRepository.java
+++ api/src/main/java/med/voll/api/repository/PacienteRepository.java
@@ -3,6 +3,7 @@ package med.voll.api.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import jakarta.validation.constraints.NotNull;
 import med.voll.api.domain.paciente.Paciente;
@@ -11,5 +12,11 @@ public interface PacienteRepository extends JpaRepository<Paciente, Long> {
 
   Page<Paciente> findAllByAtivoTrue(Pageable paginacao);
 
-  boolean findAtivoById(@NotNull Long idPaciente);
+  @Query("""
+      SELECT p.ativo
+      FROM Paciente p
+      WHERE
+      p.id = :id
+      """)
+  boolean findAtivoById(@NotNull Long id);
 }
